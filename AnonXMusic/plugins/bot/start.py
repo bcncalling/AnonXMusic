fd3a57f7176774929ffc6ddbdf9f23f41aa94fdc
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-13 12:42:04 +0000

    Update start.py
    Signed-off-by: bcncalling <142149038+bcncalling@users.noreply.github.com>

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -33,7 +33,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_photo(
-                photo=random.choice(config.START_IMG_URL),
+                photo=random.choice(config.START_IMG_URLS),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
