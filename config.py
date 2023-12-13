968d2e528aec6ca1a2533a97077d2d5d1d5fa678
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-13 12:02:41 +0000

    Update config.py
    Signed-off-by: bcncalling <142149038+bcncalling@users.noreply.github.com>

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/3f6617a56e80ae381273c.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/ab1269e3fa7b821749048.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
