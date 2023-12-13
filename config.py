697b007f6bede1a18c0eea61f6097434bc58caac
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-13 07:41:35 +0000

    Update config.py
    Signed-off-by: bcncalling <142149038+bcncalling@users.noreply.github.com>

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 LOGGER_ID = int(getenv("LOGGER_ID", "-1002097928850"))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6475535568))
+OWNER_ID = int(getenv("OWNER_ID", 5991943478))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/+Yc04IjINcqdhN2U1")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TgBotsNetwork")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+Yc04IjINcqdhN2U1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Wife_Swapping_Indiaa_group")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
