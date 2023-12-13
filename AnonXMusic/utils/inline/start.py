4156fdb015a3927b9a49551736c42ecba5519144
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-13 11:58:35 +0000

    Update start.py
    Signed-off-by: bcncalling <142149038+bcncalling@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -27,11 +27,11 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url="https://t.me/+Yc04IjINcqdhN2U1"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/Wife_Swapping_Indiaa_group"),
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/Wife_Swapping_Indiaa_group"),
+            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/+uo9AURmVEQ85OGE1"),
         ],
     ]
     return buttons
